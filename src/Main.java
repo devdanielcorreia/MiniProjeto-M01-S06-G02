5c5cc00545b2271788341f1bb7372ccaf443d9b9
Yamagishi FMT <thiago_yamagishi@hotmail.com>
2024-02-13 21:55:11 -0300

    [M1S06] Ex 1 - Gitflow  - Commit incial de criação de branch para feature do exercicio.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Commit inicial develop.");
+        System.out.println("Commit inicial branch feature.");
     }
 }
