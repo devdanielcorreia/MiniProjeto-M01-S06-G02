8487997483399a0de9711402d4d79e71aaa1db89
gustavobdambros <gustavo_dambros1@hotmail.com>
2024-02-14 19:43:08 -0300

    [M1S06] Ex 4 - Interface e Herança

---
+++ src/Funcionario.java
@@ -0,0 +1,11 @@
+public class Funcionario {
+
+    // Atributos
+    private String nome;
+    private double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+}
---
+++ src/IFuncionario.java
@@ -0,0 +1,6 @@
+public interface IFuncionario {
+
+    static void promover() {
+
+    }
+}
