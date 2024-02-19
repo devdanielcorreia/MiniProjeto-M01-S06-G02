0a99fce09e9cd69e78a0ca3a4648f2ee2a448642
Yamagishi FMT <thiago_yamagishi@hotmail.com>
2024-02-18 22:45:23 -0300

    [M1S06] Ex 9 - Fluxo de login  - Criado classe de serviço de login e implementadas as lógicas e menus de login para alunos e funcionarios;  - Implementadas lógicas de cadastro de novo usuário e login de usuário já existente;  - Criada classe utilitaria para auxiliar na exibição de menus;  - Criada classe utilitaria para validação de inputs;  - Ajustada lógica das classes de dados para implementar a serialização dos objetos e persistir os dados em arquivos localmente, simulando banco de dados;  - Ajustada lógica de verificação dos dados durante os cadastros;  - Realizada organização dos arquivos por packages de acordo com a utilidade de cada classe;  - Removidos testes anteriores da classe main;

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
+import service.LoginService;
+
 import java.util.Scanner;
+
+import static utils.ConsoleUtils.printLogoFMT;
+import static utils.ConsoleUtils.printLogoFullStack;
 
 public class Main {
     public static void main(String[] args) {
-        // System.out.println("Commit inicial branch feature.");
 
-        // Criando objetos para poder testar algumas funcionalidades
+        printLogoFMT();
+        printLogoFullStack();
+        System.out.println("\nMINI PROJETO DO MODULO 1 - SEMANA 6");
+
         Scanner entrada = new Scanner(System.in);
-        List<Curso> listaCursos = new ArrayList<>();
-        List<Aluno> listaAlunos = new ArrayList<>();
 
-        Professor professor1 = new Professor("Gabriel", 2500, CargoFuncionario.INICIANTE, 32, 5);
-        Professor professor2 = new Professor("André", 3000, CargoFuncionario.AVANCADO, 35, 7);
-
-        Curso curso1 = new Curso("Java", professor1);
-        Curso curso2 = new Curso("Python", professor2);
-
-        Aluno aluno1 = new Aluno("Rafael", 20, listaCursos, StatusMatricula.ATIVO);
-        Aluno aluno2 = new Aluno("Miguel", 24, listaCursos, StatusMatricula.ATIVO);
-
-        listaAlunos.add(aluno1);
-        listaAlunos.add(aluno2);
-
-        Turma turma1 = new Turma(listaAlunos, 2022, curso1);
-
-        aluno1.adicionarCurso(curso1);
-        aluno2.adicionarCurso(curso2);
-
-        aluno1.listarCursos();
-        aluno1.listarCursosComIndice();
-
-        aluno2.listarCursos();
-        aluno2.removerCurso(entrada); // usando o método sobrecarregado
-        aluno2.listarCursosComIndice();
-
-        turma1.listarAlunos();
-        turma1.listarAlunosComIndice();
-
+        LoginService loginService = new LoginService();
+        loginService.iniciaFluxoLogin(entrada);
     }
 }
---
+++ src/service/LoginService.java
@@ -0,0 +1,190 @@
+package service;
+
+import database.DadosAlunos;
+import database.DadosDiretores;
+import database.DadosProfessores;
+import model.Aluno;
+import model.Diretor;
+import model.Professor;
+
+import java.util.List;
+import java.util.Scanner;
+
+import static utils.ConsoleUtils.*;
+import static utils.ValidaEntradaUtils.validaInputUsuarioRangeOpcoes;
+
+public class LoginService {
+
+    private static final String TIPO_ALUNO = "aluno";
+    private static final String TIPO_DIRETOR = "diretor";
+
+    private DadosAlunos dadosAlunos = new DadosAlunos();
+    private DadosProfessores dadosProfessores = new DadosProfessores();
+    private DadosDiretores dadosDiretores = new DadosDiretores();
+
+    public void iniciaFluxoLogin(Scanner scanner) {
+
+        while (true) {
+
+            menuPrincipalLogin();
+
+            int tipoUsuario = validaInputUsuarioRangeOpcoes(scanner, 0, 2);
+
+            switch (tipoUsuario) {
+                case 0:
+                    encerrarPrograma(scanner);
+                    return;
+                case 1:
+                    fluxoLoginFuncionario(scanner);
+                    break;
+                case 2:
+                    fluxoLoginAluno(scanner);
+                    break;
+                default:
+                    System.out.println("Comando inválido. Use um dos comandos informados anteriormente. \n");
+            }
+
+        }
+    }
+
+    private void encerrarPrograma(Scanner scanner) {
+        System.out.println("Saindo do programa. \n");
+        scanner.close();
+        System.exit(0);
+    }
+
+    private void fluxoLoginAluno(Scanner scanner) {
+        boolean loginBemSucedido = false;
+
+        while (!loginBemSucedido) {
+            limparConsole();
+            menuLoginAluno();
+
+            int operacao = validaInputUsuarioRangeOpcoes(scanner, 0, 2);
+
+            switch (operacao) {
+                case 1:
+                    dadosAlunos.adicionarAluno();
+                    loginBemSucedido = true;
+                    break;
+                case 2:
+                    loginBemSucedido = fluxoLoginUsuarioExistente(scanner, dadosAlunos.getListaAlunos(), "aluno");
+                    break;
+                case 0:
+                    return;
+            }
+        }
+    }
+
+    private void fluxoLoginFuncionario(Scanner scanner) {
+        while (true) {
+            limparConsole();
+            menuLoginFuncionario();
+
+            int opcao = validaInputUsuarioRangeOpcoes(scanner, 0, 2);
+
+            switch (opcao) {
+                case 1:
+                    fluxoLoginDiretor(scanner);
+                    return;
+                case 2:
+                    fluxoLoginProfessor(scanner);
+                    return;
+                case 0:
+                    return;
+            }
+        }
+    }
+
+    private void fluxoLoginDiretor(Scanner scanner) {
+        boolean loginBemSucedido = false;
+
+        while (!loginBemSucedido) {
+            limparConsole();
+            menuLoginDiretor();
+
+            int operacao = validaInputUsuarioRangeOpcoes(scanner, 0, 2);
+
+            switch (operacao) {
+                case 1:
+                    dadosDiretores.adicionarDiretor();
+                    loginBemSucedido = true;
+                    break;
+                case 2:
+                    loginBemSucedido = fluxoLoginUsuarioExistente(scanner, dadosDiretores.getListaDiretores(), "diretor");
+                    break;
+                case 0:
+                    return;
+            }
+        }
+    }
+
+    private void fluxoLoginProfessor(Scanner scanner) {
+        boolean loginBemSucedido = false;
+
+        while (!loginBemSucedido) {
+            limparConsole();
+            menuLoginProfessor();
+
+            int operacao = validaInputUsuarioRangeOpcoes(scanner, 0, 2);
+
+            switch (operacao) {
+                case 1:
+                    dadosProfessores.adicionarProfessor();
+                    loginBemSucedido = true;
+                    break;
+                case 2:
+                    loginBemSucedido = fluxoLoginUsuarioExistente(scanner, dadosProfessores.getListaProfessores(), "professor");
+                    break;
+                case 0:
+                    return;
+            }
+        }
+    }
+
+    private boolean fluxoLoginUsuarioExistente(Scanner scanner, List<?> listaUsuarios, String tipoUsuario) {
+        exibirUsuariosExistente(listaUsuarios, tipoUsuario);
+
+        while (true) {
+            System.out.println("Digite o número do " + tipoUsuario + " que deseja selecionar (ou '0' para voltar): ");
+            int opcao = validaInputUsuarioRangeOpcoes(scanner, 0, listaUsuarios.size());
+
+            if (opcao == 0) {
+                return false;
+            }
+
+            System.out.println("Login realizado com sucesso para o " + tipoUsuario + ": " + listaUsuarios.get(opcao - 1) + "\n");
+            return true;
+        }
+    }
+
+    private void exibirUsuariosExistente(List<?> listaUsuarios, String tipoUsuario) {
+        System.out.println("Selecione o usuário para logar no sistema: \n");
+        if (tipoUsuario.equals(TIPO_ALUNO)) {
+            List<Aluno> listaAlunos = (List<Aluno>) listaUsuarios;
+            for (int i = 0; i < listaAlunos.size(); i++) {
+                System.out.println((i + 1) + " - " + listaAlunos.get(i).getNome() +
+                        " - " + listaAlunos.get(i).getIdade() + " anos" +
+                        " - Matrícula " + listaAlunos.get(i).getStatusMatricula());
+            }
+        } else if (tipoUsuario.equals(TIPO_DIRETOR)) {
+            List<Diretor> listaDiretores = (List<Diretor>) listaUsuarios;
+            for (int i = 0; i < listaDiretores.size(); i++) {
+                System.out.println((i + 1) + " - " + listaDiretores.get(i).getNome() +
+                        " - " + listaDiretores.get(i).getTempoDeCargo() + " ano de cargo" +
+                        " - R$ " + listaDiretores.get(i).getSalario());
+            }
+        } else {
+            List<Professor> listaProfessores = (List<Professor>) listaUsuarios;
+            for (int i = 0; i < listaUsuarios.size(); i++) {
+                System.out.println((i + 1) + " - " + listaProfessores.get(i).getNome() +
+                        " - " + listaProfessores.get(i).getIdade() + " anos" +
+                        " - " + listaProfessores.get(i).getTempoTrabalho() + " ano de cargo" +
+                        " - R$ " + listaProfessores.get(i).getSalario());
+            }
+        }
+
+    }
+
+
+}
